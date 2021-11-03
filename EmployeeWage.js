8dc15440e76b911c232c7d544d1ba7ebac50f2dd
atif00 <mohammedatif90@gmail.com>
2021-11-03 16:39:30 +0530

    Calculating employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,7 +1,31 @@
 console.log('Welcome to Employee Wage');
+
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
 const IS_PRESENT = 1;
 let employeeCheck = Math.floor(Math.random() * 10 % 2);
 if (employeeCheck == IS_PRESENT) {
     console.log("Employee Is Present");
-} else
+} 
+else {
     console.log("Employee Is Absent");
+}
+let empCheck = Math.floor(Math.random() * 10 % 3);
+let empHrs;
+    switch (empCheck) {
+        case IS_PART_TIME:
+            empHrs = PART_TIME_HRS;
+            break;
+        case IS_FULL_TIME:
+            empHrs = FULL_TIME_HRS;
+            break;
+        default:
+            empHrs = 0;
+    }
+    
+    let empWage = empHrs * WAGE_PER_HR;
+    console.log("Employee Wage is: "+empWage);
