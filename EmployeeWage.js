a86e6f9d1e8ed5ba12948d239e7b733ed7e3f981
atif00 <mohammedatif90@gmail.com>
2021-11-03 18:37:56 +0530

    Daily wage map and Daily Hour map to calc total wage… total hours worked,full,part and non working days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -142,3 +142,34 @@ let fullTimeWorkedDays = empDailyWageArray.reduce(totalPartTimesWorked, 0);
 console.log("Employee Worked Full time for " + fullTimeWorkedDays + " Days");
 let totalDaysWorked= fullTimeWorkedDays+partTimeWorkedDays;
 console.log("Employee Total Worked  for " + totalDaysWorked + " Days");
+
+//UC_9
+const findTotal = (totalVal, dailyVal)=>{
+    return totalVal + dailyVal;
+    }
+    
+    let count = 0;
+    
+    let totalHours = Array
+                    .from(empDailyWageMap.values())
+                    .reduce(findTotal, 0);
+    let totalSalary = empDailyWageArray
+                    . filter(dailyWage => dailyWage > 0)
+                    .reduce(findTotal,0);
+    
+    console. log("UC9 - Emp Wage with Arrow.: " + " Total Hours:"+
+                        totalHours + " Total Wages:" + totalSalary);
+    
+    let nonWorkingDays = new Array();
+    let partWorkingDays = new Array();
+    let fullWorkingDays = new Array();
+    
+    empDailyWageMap.forEach( (value, key, map) => {
+         if (value == 8) fullWorkingDays.push(key);
+        else if (value == 4) partWorkingDays.push(key); 
+        else nonWorkingDays.push(key);
+     });
+      
+    console. log("Full Working Days: "+fullWorkingDays);
+    console. log("Part Working Days: "+partWorkingDays);
+    console. log("Non Working Days: "+nonWorkingDays);
