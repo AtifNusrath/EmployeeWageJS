9169493f24887e5d5776723e05365a3394e0d702
atif00 <mohammedatif90@gmail.com>
2021-11-03 21:00:26 +0530

    Validate Pin Code

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -6,6 +6,8 @@ class EmployeePayrollData{
         this.salary=params[2];
         this.gender=params[3];
         this.startDate=params[4];
+        this.email=params[5];
+        this.zip=params[6];
     }
 
     get name() {
@@ -65,26 +67,48 @@ class EmployeePayrollData{
         else throw new Error('InValid startDate');
     }
 
+    get email(){
+        return  this._email;
+     }
+     set email(email){
+         let regEmail=RegExp('^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[(com)|(co)|(net)]+(?:\\.[a-z]{2,}){0,1}$');
+         if(regEmail.test(email)){
+             this._email=email;
+         }
+         else throw new Error ('InValid Email');
+     }
+     get zip(){
+         return this._zip;
+     }
+     set zip(zip){
+         let regZip=RegExp('^\\d{3}(\\s{0,1}\\d{3})$');
+         if(regZip.test(zip)){
+             this._zip=zip;
+         }
+         else throw new Error ('Invalid Zip');
+     }
+
     toString(){
         const options={year: 'numeric ',month: 'long',day:'numeric'};
         const empDate=!this.startDate?"unidefined":
         this.startDate.toLocalDateString("en-US",options);
         return "id = "+this.id+" , name= "+this._name+" , salary = "
         +this.salary+
-        " , "+"gender = "+this.gender +" , startDate= "+empDate;
+        " , "+"gender = "+this.gender +" , startDate= "+empDate+
+        " , "+" email: "+this.email+" pincode: "+this.zip;
     }
 }
 try{
-    let newEmployeePayrollData=new EmployeePayrollData(1,"Terrisa",30000,"M",new Date('2020-09-13'));
+    let newEmployeePayrollData=new EmployeePayrollData(1,"Terrisa",30000,"M",new Date('2020-09-13'),'terrisa@gmail.com',342014);
     console.log(newEmployeePayrollData);
 
-    let newEmployeePayrollData1=new EmployeePayrollData(1,"Terrisa",30000,"male",new Date('2021-09-13'));
+    let newEmployeePayrollData1=new EmployeePayrollData(1,"Terrisa",30000,"male",new Date('2021-09-13'),'Terrisa@gmail.com','A342014');
     console.log(newEmployeePayrollData1);
 
-    let newEmployeePayrollData2=new EmployeePayrollData(0,"Terrisa",30000,"M",new Date());
+    let newEmployeePayrollData2=new EmployeePayrollData(3,"Terrisa",30000,"M",new Date('2021-09-13'),'Terrisa@gmail.com','342014B');
     console.log(newEmployeePayrollData2);
 
-    let newEmployeePayrollData3=new EmployeePayrollData(2,"Terrisa",0,"m",new Date());
+    let newEmployeePayrollData3=new EmployeePayrollData(2,"Terrisa",52,"m",new Date('2021-09-13'),'Terrisa@gmail.com','342 014');
     console.log(newEmployeePayrollData3);
 }catch(e){
     console.error(e);
