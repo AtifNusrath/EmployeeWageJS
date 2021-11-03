50db74e1ce59547584140f85484594e9e7489869
atif00 <mohammedatif90@gmail.com>
2021-11-03 17:13:00 +0530

    Storing the Daily Wage in array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -9,6 +9,10 @@ const WAGE_PER_HR = 20;
 const IS_PRESENT = 1;
 const MAX_WORKING_DAYS = 20;
 const MAX_WORKING_HOURS = 160;
+let totalEmpHrs = 0;
+let totalWorkigDays = 0;
+let empDailyWageArray = new Array();
+
 
 /*let employeeCheck = Math.floor(Math.random() * 10 % 2);
 if (employeeCheck == IS_PRESENT) {
@@ -28,15 +32,21 @@ function getEmployeeWage(eCheck){
             return 0;
     }
 }
-let totalEmpHrs = 0;
-let totalWorkigDays = 0;
+
+function calculateWage(empHrs) {
+    return empHrs *  WAGE_PER_HR;
+}
+
 while (totalEmpHrs < MAX_WORKING_HOURS && totalWorkigDays < MAX_WORKING_DAYS) {
+    totalWorkigDays++;
     let employeeCheck = Math.floor(Math.random() * 10) % 3;
     let emphrs = getEmployeeWage(employeeCheck);
     totalEmpHrs += emphrs;
-    totalWorkigDays++;
+    empDailyWageArray.push(calculateWage(emphrs)); 
+    
 }
 
-let totalEmpWage = totalEmpHrs * WAGE_PER_HR;
+let totalEmpWage = calculateWage(totalEmpHrs);
+console.log("Daily Employee Wage stored in Array:\n"+empDailyWageArray);
 console.log(" Total Employee Wage is: " + totalEmpWage + " for Total Employee Work Hours: " + totalEmpHrs
 +" Total Working Days: "+totalWorkigDays);
